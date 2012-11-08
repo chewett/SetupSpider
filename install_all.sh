07fe9a9450dac0f633b356c7fbc8ed8819d173b4
Christopher Hewett <chewett@hotmail.co.uk>
2012-11-08 13:18:35 +0000

    removed netbeans since its not in f17 repo apparently

--- install_all.sh
+++ install_all.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 yum install git geany gimp inkscape calibre pidgin thunderbird xchat gnucash libreoffice eclipse mysql_workbench apache mysql php banshee terminator
-
-yum install netbeans-platform
