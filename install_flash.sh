f1a8bf312ad3558bba28a2e9a9a1d1c56eaf9cae
Christopher Hewett <chewett@hotmail.co.uk>
2012-11-08 13:36:16 +0000

    Added flash install script

---
+++ install_flash.sh
@@ -0,0 +1,3 @@
+su -c 'rpm -ivh adobe-release-x86_64-1.0-1.noarch.rpm'
+su -c 'rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux'
+su -c 'yum install nspluginwrapper alsa-plugins-pulseaudio flash-plugin'
