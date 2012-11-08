e974f6873091cd8f026c3d23c23f93c803139130
Christopher Hewett <chewett@hotmail.co.uk>
2012-11-08 13:43:10 +0000

    Added mysql-workbench and removed the invalid named yum call to it

--- install_all.sh
+++ install_all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-yum install git geany gimp inkscape calibre pidgin thunderbird xchat gnucash libreoffice eclipse mysql_workbench apache mysql php banshee terminator
+yum install git geany gimp inkscape calibre pidgin thunderbird xchat gnucash libreoffice eclipse apache mysql php banshee terminator
 
 yum install vlc
 yum install texmaker
 yum install p7zip p7zip-plugins
 yum install filezilla
+yum install mysql-workbench
