4f4c089ad6dfc5401512a0cd10ccd7f1722bfa59
Christopher Hewett <chewett@hotmail.co.uk>
2012-11-08 13:38:47 +0000

    Added texmaker to the install all command file

--- install_all.sh
+++ install_all.sh
@@ -2,3 +2,4 @@
 yum install git geany gimp inkscape calibre pidgin thunderbird xchat gnucash libreoffice eclipse mysql_workbench apache mysql php banshee terminator
 
 yum install vlc
+yum install texmaker
