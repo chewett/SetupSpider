5796e08f2cbb473b1f4f1e7bfe6e8948e371c53f
Christopher Hewett <chewett@hotmail.co.uk>
2012-11-15 11:40:52 +0000

    Added -y prefix for yum so it doesnt ask any questions before installing

--- install_all.sh
+++ install_all.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 yum update
 
-yum install git
-yum install git geany gimp inkscape calibre pidgin thunderbird 
-yum install xchat gnucash libreoffice eclipse mysql banshee terminator
-yum install htop
-yum install vlc
-yum install p7zip p7zip-plugins
-yum install filezilla
-yum install mysql-workbench
-yum install ipython
-yum install svn
-yum install josm
-yum install python-bottle
+yum install -y git
+yum install -y git geany gimp inkscape calibre pidgin thunderbird 
+yum install -y xchat gnucash libreoffice eclipse mysql banshee terminator
+yum install -y htop
+yum install -y vlc
+yum install -y p7zip p7zip-plugins
+yum install -y filezilla
+yum install -y mysql-workbench
+yum install -y ipython
+yum install -y svn
+yum install -y josm
+yum install -y python-bottle
 
-yum install 0ad wesnoth
+yum install -y 0ad wesnoth
