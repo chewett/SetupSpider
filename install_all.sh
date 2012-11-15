34903384e7b63d229c30f78d5de7939d614dd523
Christopher Hewett <chewett@hotmail.co.uk>
2012-11-15 11:27:56 +0000

    removed installing http stuff in install_all script

--- install_all.sh
+++ install_all.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 yum update
 
-yum install mysql mysql-server
 yum install git
 yum install git geany gimp inkscape calibre pidgin thunderbird 
 yum install xchat gnucash libreoffice eclipse mysql banshee terminator
-
-yum install php php-mysql
 yum install htop
 yum install vlc
 yum install texmaker
 yum install p7zip p7zip-plugins
 yum install filezilla
 yum install mysql-workbench
-yum install httpd
 yum install ipython
 yum install svn
 yum install josm
