c572886281696ec4357cfd901ca8acd1305bee3b
Christopher Hewett <chewett@hotmail.co.uk>
2012-11-15 11:09:25 +0000

    Removed duplicate nonfree repo setup script

--- setup-nonfree-repo.sh
+++
@@ -1,6 +0,0 @@
-
-
-
-yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm
-
-
