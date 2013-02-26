aa6878229fd624dd8eb8ebd564e63939e4b33631
Christopher Hewett <chewett@hotmail.co.uk>
2013-02-26 11:24:02 +0000

    Fixed up the script to allow easier disabling

--- install_all.sh
+++ install_all.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 yum update
 
+#core stuff used
 yum install -y vim
 yum install -y screen
+yum install -y htop
+yum install -y terminator
+yum install -y svn
+yum install -y p7zip p7zip-plugins
 yum install -y git
+
 yum install -y geany 
 yum install -y gimp inkscape 
-yum install -y calibre pidgin thunderbird 
-yum install -y xchat gnucash libreoffice eclipse mysql banshee terminator
-yum install -y htop
-yum install -y vlc
-yum install -y p7zip p7zip-plugins
-yum install -y filezilla
+yum install -y calibre
+yum install -y pidgin
+yum install -y thunderbird 
+yum install -y xchat 
+yum install -y gnucash 
+yum install -y libreoffice
+
+#web stuff
+yum install -y eclipse 
+yum install -y mysql
 yum install -y mysql-workbench
+yum install -y filezilla
+
+yum install -y banshee
+yum install -y vlc
 yum install -y ipython
-yum install -y svn
 yum install -y josm
 yum install -y python-bottle
 
+# games
 yum install -y 0ad wesnoth
