3d2cdb3bdebdbaf3e5e05ad93797b208ad7cc46c
Christopher Hewett <chewett@hotmail.co.uk>
2013-02-26 09:44:47 +0000

    changed file to include better information

--- install_flash.sh
+++ install_flash.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-wget http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
 
-rpm -ivh adobe-release-x86_64-1.0-1.noarch.rpm
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
+#http://www.ubuntugeek.com/howto-enable-flash-support-for-google-chromium-browser.html
+
 yum install -y nspluginwrapper alsa-plugins-pulseaudio flash-plugin
