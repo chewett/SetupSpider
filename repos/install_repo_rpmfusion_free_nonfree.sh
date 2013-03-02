ef4352adb66c5713c7b583ef06b859f4dd0cfbd3
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-02 12:54:05 +0000

    Added rpm fusion for fc18

--- repos/install_repo_rpmfusion_free_nonfree.sh
+++ repos/install_repo_rpmfusion_free_nonfree.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-su -c 'yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm'
+sudo yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-18.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-18.noarch.rpm
