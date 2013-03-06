1aa6dfe097062acaac7ea6c3063b821767f30810
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-06 22:07:32 +0000

    Added bumblebee setup for fc18 and a local copy

--- install_bumblebee.sh
+++ install_bumblebee.sh
@@ -4,12 +4,11 @@
 # NOTE
 #####################
 #
-# This works mostlfy for fedora 17 Dell XPS L502x and will need changes
+# This works mostlfy for fedora 18 Dell XPS L502x and will need changes
 #
 # These instructions were found at:
 # http://techies.ncsu.edu/wiki/bumblebee
 #
 
-yum -y --nogpgcheck install http://install.linux.ncsu.edu/pub/yum/itecs/public/bumblebee-nonfree/fedora17/noarch/bumblebee-nonfree-release-1.0-1.noarch.rpm
-yum -y --nogpgcheck install http://install.linux.ncsu.edu/pub/yum/itecs/public/bumblebee/fedora17/noarch/bumblebee-release-1.0-1.noarch.rpm
+yum -y --nogpgcheck install http://install.linux.ncsu.edu/pub/yum/itecs/public/bumblebee-nonfree/fedora18/noarch/bumblebee-nonfree-release-1.0-1.noarch.rpm
 yum -y install bumblebee-nvidia
