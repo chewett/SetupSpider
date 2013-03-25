438986f44633c6a851d9e21c0ebb0c31a604f52d
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-25 22:36:04 +0000

    Added ability to install all packages passed to it

--- SetupAutomator.py
+++ SetupAutomator.py
@@ -4,3 +4,8 @@ class SetupAutomator():
 
     def install(self, package):
         subprocess.call(["sudo", "yum", "install", package])
+
+    def install_all(self, packages):
+        cmd = ["sudo", "yum", "install"] + packages
+
+        subprocess.call(cmd)
