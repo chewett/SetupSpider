5ad477b34091a4a35ebf088d1636c244370ab6fc
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-25 22:59:16 +0000

    Moved instakk_games to python file

---
+++ install_games.py
@@ -0,0 +1,13 @@
+#!/bin/python
+from SetupAutomator import *
+
+sa = SetupAutomator()
+sa.update() #update before trying to install new packages
+
+all_packages = []
+
+all_packages.append("oad")
+all_packages.append("wesnoth")
+
+#now install all listed packages
+sa.install_all(all_packages)
--- install_games.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo yum install 0ad wesnoth
