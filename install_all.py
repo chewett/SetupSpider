4da92ba2c4760dd5407ee3fe7322fd9c8f6bb074
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-27 12:14:56 +0000

    Fixed caps on virtualbox so it installs

--- install_all.py
+++ install_all.py
@@ -31,7 +31,7 @@ all_packages.append("gnucash")
 all_packages.append("libreoffice")
 all_packages.append("vlc")
 all_packages.append("youtube-dl")
-all_packages.append("virtualbox")
+all_packages.append("VirtualBox")
 
 #web stuff
 all_packages.append("eclipse")
