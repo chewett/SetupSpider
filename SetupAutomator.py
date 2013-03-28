46fc3506cc4b4909c38c68ff0769c2cd094da339
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-28 19:54:58 +0000

    Added ability to use apt to install packages

--- SetupAutomator.py
+++ SetupAutomator.py
@@ -2,13 +2,23 @@ import subprocess
 
 class SetupAutomator():
 
+    def __init__(self, install_type="yum"):
+        self.install_type = install_type
+
+    def _get_install_cmd(self):
+        if self.install_type == "yum":
+            return ["sudo", "yum", "install"]
+        elif self.install_type == "apt":
+            return ["sudo", "apt-get", "install"]
+        else:
+            exit("Unknown install type")
+
+
     def install(self, package):
-        subprocess.call(["sudo", "yum", "install", package])
+        subprocess.call(self._get_install_cmd().append(package))
 
     def install_all(self, packages):
-        cmd = ["sudo", "yum", "install"] + packages
-
-        subprocess.call(cmd)
+        subprocess.call(self._get_install_cmd() + packages)
 
     def update(self):
         subprocess.call(["sudo", "yum", "update"])
