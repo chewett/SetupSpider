7d137e100125dbfd0968fd32274b11b8dc868dc2
Christopher Hewett <chewett@hotmail.co.uk>
2013-03-28 19:58:48 +0000

    Added an install_all for raspberry pi

---
+++ raspberry_pi/install_all.py
@@ -0,0 +1,15 @@
+#!/bin/python
+import sys
+sys.path.append("../")
+from SetupAutomator import *
+
+sa = SetupAutomator()
+sa.update() #update before trying to install new packages
+
+all_packages = []
+
+all_packages.append("oad")
+all_packages.append("wesnoth")
+
+#now install all listed packages
+sa.install_all(all_packages)
