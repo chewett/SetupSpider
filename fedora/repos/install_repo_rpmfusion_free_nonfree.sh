09922885c23dbf1c8917162275a369657cdfe53f
Christopher Hewett <chewett@hotmail.co.uk>
2013-07-20 14:06:03 +0100

    Added rpmfusion for FC19

--- fedora/repos/install_repo_rpmfusion_free_nonfree.sh
+++ fedora/repos/install_repo_rpmfusion_free_nonfree.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-18.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-18.noarch.rpm
+sudo yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-19.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-19.noarch.rpm
