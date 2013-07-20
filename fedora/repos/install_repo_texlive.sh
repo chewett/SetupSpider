b44f64cf30fe1b9ecbfd40552efe052513d4d92e
Christopher Hewett <chewett@hotmail.co.uk>
2013-07-20 14:07:23 +0100

    Added correct url for fc18

--- fedora/repos/install_repo_texlive.sh
+++ fedora/repos/install_repo_texlive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rpm -i http://jnovy.fedorapeople.org/texlive/2012/packages.fc17/texlive-release.noarch.rpm
+rpm -i http://jnovy.fedorapeople.org/texlive/2012/packages.fc18/texlive-release.noarch.rpm
