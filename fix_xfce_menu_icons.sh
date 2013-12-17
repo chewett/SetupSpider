f9e2d506afdc5240118bd0edfbd7484a00a4fac9
Christopher Hewett <chewett@hotmail.co.uk>
2013-12-17 22:26:36 +0000

    Removed script that isnt needed anymore

--- fix_xfce_menu_icons.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#used to make the xfce menu items show their icons
-
-gconftool-2 --type boolean --set /desktop/gnome/interface/buttons_have_icons true
-gconftool-2 --type boolean --set /desktop/gnome/interface/menus_have_icons true
