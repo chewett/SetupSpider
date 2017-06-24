c814938fe32226045f7569b5488c40f02a6d519b
Christopher Hewett <chewett@hotmail.co.uk>
2017-06-24 14:10:11 +0100

    Modified how I will use the file

--- manage_setup_files.py
+++ manage_setup_files.py
@@ -2,19 +2,16 @@ import os
 import json
 import argparse
 
-
-
-parser = argparse.ArgumentParser(description='Used to configure setup files')
-parser.add_argument('--create', help='Location to create a repo file in')
+parser = argparse.ArgumentParser(description='Used to set up config files')
+parser.add_argument('create', help='Location to create a repo file in')
 
 args = parser.parse_args()
 
-if args.create:
-    create_file = args.create
-    if not os.path.isfile(create_file):
-        with open(create_file, "w") as package_file:
-            json.dump({"package_sets": {}}, package_file)
-        print "Created file at", create_file
+create_file = args.create
+if not os.path.isfile(create_file):
+    with open(create_file, "w") as package_file:
+        json.dump({"package_sets": {}}, package_file)
+    print "Created file at", create_file
 
-    else:
-        print "Warning: file already exists, not creating"
+else:
+    print "Warning: file already exists, not creating"
